c2c4b2dc9372c458c671012243e03f7aa15422f9
zhanarys.kit <zh.sayranbekov@kit.systems>
2020-08-18 23:15:29 +0600

    Skew

---
+++ src/images/images.js
@@ -0,0 +1,9 @@
+const images = [
+  'https://images.pexels.com/photos/342003/pexels-photo-342003.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  'https://images.pexels.com/photos/402675/pexels-photo-402675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  'https://images.pexels.com/photos/2859597/pexels-photo-2859597.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  'https://images.pexels.com/photos/4066828/pexels-photo-4066828.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  'https://images.pexels.com/photos/2743739/pexels-photo-2743739.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+]
+
+export default images
